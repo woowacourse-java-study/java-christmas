42edbee094991a66689dbbfd659a5bc76a6d15a9
이창근 <dlckdrms0517@gmail.com>
2024-11-21 11:52:28 +0900

    feat: Menu 생성 기능 개발

--- src/main/java/christmas/menu/Menu.java
+++ src/main/java/christmas/menu/Menu.java
@@ -1,30 +1,47 @@
 package christmas.menu;
 
-import christmas.ParamsValidator;
+/*
+<애피타이저>
+양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)
 
-public class Menu {
+<메인>
+티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)
+
+<디저트>
+초코케이크(15,000), 아이스크림(5,000)
+
+<음료>
+제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)
+ */
+public enum Menu {
 	
-	private final String name;
-	private final int cost;
-	private final MenuType menuType;
+	양송이스프(6_000, MenuType.APPETIZER),
+	타파스(5_500, MenuType.APPETIZER),
+	시저샐러드(8_000, MenuType.APPETIZER),
 	
-	public Menu(String name, int cost, MenuType menuType) {
-		ParamsValidator.validateParamsNotNull(name, menuType);
-		validate(name, cost);
-		this.name = name;
+	티본스테이크(55_000, MenuType.MAIN),
+	바비큐립(54_000, MenuType.MAIN),
+	해산물파스타(35_000, MenuType.MAIN),
+	크리스마스파스타(25_000, MenuType.MAIN),
+	
+	초코케이크(15_000, MenuType.DESSERT),
+	아이스크림(5_000, MenuType.DESSERT),
+	
+	제로콜라(5_000, MenuType.DRINK),
+	레드와인(60_000, MenuType.DRINK),
+	샴페인(25_000, MenuType.DRINK),
+	;
+	
+	public final int cost;
+	public final MenuType menuType;
+	
+	Menu(int cost, MenuType menuType) {
 		this.cost = cost;
 		this.menuType = menuType;
 	}
 	
-	private static void validate(String name, int cost) {
-		if (name.isBlank()) {
-			throw new IllegalArgumentException("메뉴 이름은 비어있을 수 없습니다.");
-		}
-		if (cost <= 0) {
-			throw new IllegalArgumentException("메뉴 가격은 0 이하가 될 수 없습니다.");
-		}
-	}
-	
 	//TODO : 특정 구매량에 대한 금액 반환
-	//TODO : 프로그램 시작시 자동으로 생성되어야 함
+	public int calculateTotalCost(int purchaseAmount) {
+		return cost * purchaseAmount;
+	}
 }
