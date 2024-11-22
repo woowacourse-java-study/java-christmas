311cce8533cb72d88a92ffa588a793a3c73d3181
이창근 <dlckdrms0517@gmail.com>
2024-11-22 18:39:59 +0900

    feat: 혜택 금액에 따른 뱃지 종류 반환 기능 개발

--- src/main/java/christmas/badge/Badge.java
+++ src/main/java/christmas/badge/Badge.java
@@ -1,6 +1,24 @@
 package christmas.badge;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum Badge {
+	별(5_000),
+	트리(10_000),
+	산타(20_000),
+	;
+	
+	private final int from;
+	
+	Badge(int from) {
+		this.from = from;
+	}
 	
 	//TODO : 혜택 금액에 따른 뱃지 종류 반환
+	public static Optional<Badge> from(int purchaseAmount) {
+		return Arrays.stream(Badge.values())
+				.filter(badge -> badge.from >= purchaseAmount)
+				.findFirst();
+	}
 }
