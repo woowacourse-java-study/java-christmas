97b8398855c353b9fdafcd454fe43c278289d3fb
jonghwanlee <kavae550@gmail.com>
2024-11-26 22:26:07 +0900

    feat(domain): DiscountProcessor 구현체 구현
    네 개의 DiscountProcessor.java 구현체 구현

--- src/main/java/christmas/domain/event/DiscountProcessor.java
+++ src/main/java/christmas/domain/event/DiscountProcessor.java
@@ -1,4 +1,5 @@
 package christmas.domain.event;
 
-public interface DiscountProcessor {
+public interface DiscountProcessor<Type> {
+    int processDiscount(Type input);
 }
--- src/main/java/christmas/domain/event/impl/DDayDiscount.java
+++ src/main/java/christmas/domain/event/impl/DDayDiscount.java
@@ -1,6 +1,18 @@
 package christmas.domain.event.impl;
 
 import christmas.domain.event.DiscountProcessor;
+import java.time.LocalDate;
 
-public class DDayDiscount implements DiscountProcessor {
+public class DDayDiscount implements DiscountProcessor<LocalDate> {
+    private static final LocalDate CHRISTMAS =LocalDate.of(2023,12,25);
+    @Override
+    public int processDiscount(LocalDate reservationDate) {
+        if (reservationDate.isAfter(CHRISTMAS)) {
+            return 0;
+        }
+        int discount = 1000;
+       discount += 100 * (reservationDate.getDayOfMonth() - 1);
+
+        return discount;
+    }
 }
