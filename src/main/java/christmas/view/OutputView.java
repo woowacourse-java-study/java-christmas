29ab7fd0124f1bbb2aabb13c97d4fbd651106944
jonghwanlee <kavae550@gmail.com>
2024-11-27 00:29:18 +0900

    feat(view): view 관련 클래스 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,8 @@
 package christmas.view;
 
+import christmas.dto.ReceiptDTO;
+
 public interface OutputView {
+    void printWelcomeMessage();
+    void printReceipt(ReceiptDTO receiptDTO);
 }
--- src/main/java/christmas/view/impl/ConsoleOutputView.java
+++ src/main/java/christmas/view/impl/ConsoleOutputView.java
@@ -1,6 +1,15 @@
 package christmas.view.impl;
 
+import christmas.dto.ReceiptDTO;
 import christmas.view.OutputView;
 
 public class ConsoleOutputView implements OutputView {
+
+    public void printWelcomeMessage(){
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printReceipt(ReceiptDTO receiptDTO) {
+        System.out.println(receiptDTO.receipt());
+    }
 }
