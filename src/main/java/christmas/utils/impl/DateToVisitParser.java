5f479befd6201d1bd3c39dbd1b5ff4c07ef5654c
jonghwanlee <kavae550@gmail.com>
2024-11-27 16:42:28 +0900

    fix: DateToVisitParser 로직 상 오류 수정

--- src/main/java/christmas/utils/impl/DateToVisitParser.java
+++ src/main/java/christmas/utils/impl/DateToVisitParser.java
@@ -2,8 +2,9 @@ package christmas.utils.impl;
 
 import christmas.utils.InputParser;
 import christmas.utils.InputValidator;
+import java.time.LocalDate;
 
-public class DateToVisitParser implements InputParser<Integer> {
+public class DateToVisitParser implements InputParser<LocalDate> {
     private final InputValidator dateToVisitValidator;
 
     public DateToVisitParser(InputValidator dateToVisitValidator) {
@@ -11,8 +12,9 @@ public class DateToVisitParser implements InputParser<Integer> {
     }
 
     @Override
-    public Integer parse(String rawDate) {
+    public LocalDate parse(String rawDate) {
         dateToVisitValidator.validate(rawDate);
-        return Integer.parseInt(rawDate);
+        int day =  Integer.parseInt(rawDate);
+        return LocalDate.of(2023, 12, day);
     }
 }
